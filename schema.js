695643ba7280e510ad45284c52c0fc4a29bb3201
Leonardo Graboski Veiga <leogveiga@gmail.com>
2018-12-12 17:23:22 -0200

    Fix comment syntax
    Just misplaced hashtags instead of double slashes. Using too much
    bash...

--- schema.js
+++ schema.js
@@ -1,7 +1,7 @@
-# Create object
+// Create object
 var xhr = new XMLHttpRequest();
 
-# Constants
+// Constants
 var url = https://prod-85.westeurope.logic.azure.com:443/workflows/2b8d5fe1f9984b94a6bcee8e8262f199/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=ByACRiHlv8q7wTVnKW3F0-_lUijkp1us9nI7acL92k8;
 var data = {
     "description": "asdfasdf",
@@ -10,14 +10,14 @@ var data = {
     "quantity": 4.5
 }
 
-# Action
+// Action
 xhr.onreadystatechange = function() {
     if (xhr.readyState == XMLHttpRequest.DONE) {
         console.log(JSON.parse(xhr.responseText));
     }
 }
 
-# Send request
+// Send request
 xhr.setRequestHeader('Content-type','application/json; charset=utf-8');
 xhr.open("POST", url, true);
 xhr.send(data);
