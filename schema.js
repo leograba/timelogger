ab224a45fcbc319991819abc24cbf32a389f91fd
Leonardo Graboski Veiga <leogveiga@gmail.com>
2018-12-12 17:56:27 -0200

    Put MS Flow URL in a config.json file
    Make it easily configurable.

--- schema.js
+++ schema.js
@@ -1,4 +1,5 @@
 const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+const cfg = require("./config.json");
 
 function addEntryToHours(data){
     // Expect data in the following format
@@ -13,7 +14,7 @@ function addEntryToHours(data){
     var xhr = new XMLHttpRequest();
 
     // Constants
-    var url = "https://prod-85.westeurope.logic.azure.com:443/workflows/2b8d5fe1f9984b94a6bcee8e8262f199/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=ByACRiHlv8q7wTVnKW3F0-_lUijkp1us9nI7acL92k8";
+    var url = cfg.msflow_url;
     var data_json = JSON.stringify(data);
 
     // Action
