b54bc2aabe37dfd63d5ba03f9ee37ed23f0dd875
Leonardo Graboski Veiga <leogveiga@gmail.com>
2018-12-12 17:35:53 -0200

    Turn script into function that receives data
    First step into making the script usable also as a lib for an e.g. GUI.
    Makes it more flexible since data is not hardcoded anymore.

--- schema.js
+++ schema.js
@@ -1,25 +1,29 @@
-// Create object
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-var xhr = new XMLHttpRequest();
+function addEntryToHours(data){
+    // Expect data in the following format
+    /*var data = {
+        'description': 'asdfasdf',
+        'analytic_account': 'IN206',
+        'product': 'GRCost99',
+        'quantity': 4.5
+    }*/
 
-// Constants
-var url = "https://prod-85.westeurope.logic.azure.com:443/workflows/2b8d5fe1f9984b94a6bcee8e8262f199/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=ByACRiHlv8q7wTVnKW3F0-_lUijkp1us9nI7acL92k8";
-var data = {
-    "description": "asdfasdf",
-    "analytic_account": "IN206",
-    "product": "GRCost99",
-    "quantity": 4.5
-}
-var data_json = JSON.stringify(data);
+    // Create object
+    var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+    var xhr = new XMLHttpRequest();
 
-// Action
-xhr.onreadystatechange = function() {
-    if (xhr.readyState == XMLHttpRequest.DONE) {
-        console.log(JSON.parse(xhr.responseText));
+    // Constants
+    var url = "https://prod-85.westeurope.logic.azure.com:443/workflows/2b8d5fe1f9984b94a6bcee8e8262f199/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=ByACRiHlv8q7wTVnKW3F0-_lUijkp1us9nI7acL92k8";
+    var data_json = JSON.stringify(data);
+
+    // Action
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == XMLHttpRequest.DONE) {
+            console.log(JSON.parse(xhr.responseText));
+        }
     }
-}
 
-// Send request
-xhr.open("POST", url, true);
-xhr.setRequestHeader('Content-Type','application/json');
-xhr.send(data_json);
+    // Send request
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader('Content-Type','application/json');
+    xhr.send(data_json);
+}
