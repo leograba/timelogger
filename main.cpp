69e3413484f36a2a5fa14d1111e9232a821016cd
Leonardo Graboski Veiga <leogveiga@gmail.com>
2018-12-22 15:39:09 -0200

    Register readjson class as QML object

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 
+#include <readjson.h>
+
 int main(int argc, char *argv[])
 {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
     QGuiApplication app(argc, argv);
+
+    qmlRegisterType<ReadJson>("leo.readjson", 1, 0, "ReadJson");
 
     QQmlApplicationEngine engine;
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
